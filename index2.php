a3585b37d8b557011bc6e0d522829a642ac0d4c2
En Tay <rush_x@yahoo.com>
2025-11-09 21:25:04 +0700

    2025-11-09

---
+++ index2.php
@@ -0,0 +1,148 @@
+<?php include("header.php"); ?>
+
+<section id="hero" class="hero d-flex align-items-center">    
+    <div class="container position-relative" data-aos="fade-up" data-aos-delay="500" style="text-align: left; padding: 5px 5px">
+      <p class="p1" >
+        <i style="color:yellow">Kedatangan Tuhan Yesus membawa kita mengerti Allah yang tidak mungkin kita mengerti,
+        melihat Allah yang tidak mungkin kita lihat, 
+        dan menikmati kemuliaan Allah yang tidak mungkin kita mengerti.</i><br>
+        <br>
+        ~~ Pdt. Dr. Stephen Tong
+      </p>
+      <!--P class="p2" >
+        Pelayanannya juga termasuk dalam misi penginjilan, mendorong orang-orang untuk menyebarkan pesan Injil, menginspirasi banyak orang untuk terlibat aktif dalam iman Kristen dan menjadi warga yang peduli di masyarakat.
+      </p-->
+      <br>
+      <a href="#event" class="btn-get-started scrollto mb-2" style="width: 330px; font-size:22px">Natal Bandung 2024</a> <br>
+      <a href="profil.php" class="btn-get-started" style="width: 330px">Profil: Pdt. Dr. Stephen Tong</a>
+    </p>
+</section>
+
+<!-- ======= event Section ======= -->
+<section id="event" class="event">
+    <div class="container">
+
+    <div class="row">
+        <div class="col-lg-6 order-1 order-lg-1" data-aos="fade-left">
+            <!--img src="assets/img/natal2024-profile-500.jpg" class="img-fluid"-->
+            <iframe height="315" src="https://www.youtube.com/embed/gVz6oBYYQq4?si=1yOZCvcvWsh7Ohvg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="width:100%"></iframe>            
+        </div>
+        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-right">
+          <p class="fst-italic">
+              <b>Kebaktian Natal 2024 bagi kota Bandung</b>, akan kembali dilayani oleh hamba-Nya, Pdt. Stephen Tong.       
+          </p>
+          <p>
+            <strong style="font-size:20px"><i>Imanuel dalam Sejarah</i></strong><br>
+            <br>
+            📅 Senin, 2 Desember 2024. 🕡 pk. 18.30<br>
+            📌 Sudirman Grand Ballroom<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jl. Jend. Sudirman 620, Bandung
+          </p>            
+          <p> Acara ini gratis, terbuka untuk umum. Mari hadiri, ajak teman/saudara/kenalan untuk datang mendengar berita injil, berita natal.</p>        
+          <a class="scrollto btn-link" href="#lokasi">Lokasi & Titik Jemput</a>
+        </div>
+    </div>
+
+    </div>
+</section><!-- End event Section -->
+
+<section id="mentor" class="mentor">
+    <div class="container">
+      <div class="text-center"><img src="assets/img/garis.png" style="max-width:100%"></div>
+
+      <div class="section-title">
+        <!-- <span>Lokasi</span> -->
+        <h2>Our Mentor</h2>
+        <p class="text-center">Mari kita mengenal Pdt. Stephen Tong dalam seri <i>Our Mentor</i>.<br>
+        Beliau akan menyampaikan Firman di <strong>NATAL BANDUNG 2024</strong>,<br>
+        seorang pelayan Tuhan yang sungguh dipakai Tuhan.</p>
+      </div>
+        <div class="row">
+            <div class="offset-1 col-lg-5 order-1 order-lg-1" data-aos="fade-left">
+              <p>Seri <i>Our Mentor</i> ini mengangkat kisah pelayanan <a href="bio.html">Pdt. Stephen Tong</a> dari berbagai narasumber untuk membagikan insight, motivasi dan tantangan pelayanan sehingga dapat menjadi berkat bagi umat Tuhan saat ini.</p>
+              <p>Di episode ini <b>Pdt. Ivan Kristiono</b> akan mengulas aspek pendidikan Kristen dan pelayanan anak yang telah dikerjakan oleh Pdt. Stephen Tong</p>
+            </div>
+            <div class="col-lg-5 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-right">
+              <iframe height="315" src="https://www.youtube.com/embed/e9TVcsgwbbE?si=9L1oiIUkLuMarEDp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="width:100%"></iframe>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-5 order-3 order-lg-1" data-aos="fade-left">
+              <br><br>
+              <p>Episode <i>Our Mentor</i> oleh <b>Pdt. Billy Kristanto</b> yang mengulas berbagai hal yang dikerjakan
+              Pdt. Stephen Tong terkait mandat budaya.</p>
+            </div>
+            <div class="col-lg-3 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-right">
+              <iframe height="315" src="https://www.youtube.com/embed/nuvySIbLcek" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="width:100%"></iframe>            
+            </div>
+            <div class="col-lg-3 pt-4 pt-lg-0 order-3 order-lg-3 content" data-aos="fade-right">
+              <iframe height="315" src="https://www.youtube.com/embed/icoQE1DD9A4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="width:100%"></iframe>            
+            </div>
+            
+            
+            
+        </div>
+    </div>
+</section>
+
+<!-- ======= lokasi Section ======= -->
+<section id="lokasi" class="lokasi">
+  <div class="container">
+    <div class="text-center"><img src="assets/img/garis.png" style="max-width:100%"></div>
+    <div class="section-title">
+      <!-- <span>Lokasi</span> -->
+      <h2>Lokasi</h2>
+      <p class="text-center" style="font-size:18px">Jl. Jend. Sudirman No.620<br>
+        Kota Bandung, Jawa Barat 40183</p>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-7 pt-4 pt-lg-0 order-1 order-lg-1 content text-center" data-aos="fade-up">
+            <!-- iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.0965347471596!2d107.57921535545967!3d-6.917629549053815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e608cfa0c18d%3A0xbe7da28b948823b!2sSudirman%20Grand%20Ballroom!5e0!3m2!1sen!2sid!4v1699982155520!5m2!1sen!2sid"  frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe-->
+            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.7748657365473!2d107.5770186741586!3d-6.917497467703025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e608cfa0c18d%3A0xbe7da28b948823b!2sSudirman%20Grand%20Ballroom!5e0!3m2!1sen!2sid!4v1730474769066!5m2!1sen!2sid" height="350" style="border:0; width:100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+            <img src="assets/img/denah.jpg" style="width:100%; margin-bottom: 10px;">
+            <!-- <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe> -->
+            <a href="https://maps.app.goo.gl/7mtQiRmT4uvYxpJQ6" class="btn-link" target="_blank">📌 Google Maps</a>
+        </div>
+
+        <div class="col-lg-5 pt-4 pt-lg-0 order-2 order-lg-2 content" data-aos="fade-right">
+            <p>Tersedia <b>transportasi keberangkatan</b> bersama dari/ke Sudirman Grand Ballroom</p>
+            <div>TITIK JEMPUT:</div>
+            <ul>
+                <li>UPI - <a href="https://maps.app.goo.gl/aEYZJWZeQUXtAd8W9" target="_blank">[<i class="bi bi-pin-map-fill"></i> Maps]</a></li>
+                <li>ITB-Baltos - <a href="https://maps.app.goo.gl/kvBWKgg45Huv1rhr8" target="_blank">[<i class="bi bi-pin-map-fill"></i> Maps]</a></li>
+            </ul>
+            <a href="https://forms.gle/uQJ2mVPXAmAWRCeNA" class="btn-link">Daftar Transportasi Keberangkatan</a>
+        </div>
+    </div>
+    
+  </div>
+</section><!-- End lokasi Section -->
+
+<!-- ======= Contact Section ======= -->
+<section id="contact" class="contact">
+      <div class="container">
+        <div class="text-center"><img src="assets/img/garis.png" style="max-width:100%"></div>
+        <div class="section-title">
+          <!-- <span>Hubungi Kami</span> -->
+          <h2>Hubungi Kami</h2>
+          <br><br>
+          <p class="text-center">untuk informasi lain, pertanyaan atau keberangkatan bersama</p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+
+          <div class="offset-lg-4 col-lg-4 col-md-6" onclick="window.open('https://wa.me/6285105071880')">
+            <div class="info-box mb-4 text-center">
+              <i class="bx bx-phone-call"></i>
+              <h3>Telp/WhatsApp</h3>
+              <p class="mx-auto" style="color:white">0851 0507 1880</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+<?php include("footer.php"); ?>
